f4614a9ee00b8763c9acd8f8d12fa6d89934900f
Yash Garg <yash431garg@gmail.com>
2024-03-23 17:33:00 +0530

    updated frame to multiframe

--- src/app/frames/route.tsx
+++ src/app/frames/route.tsx
@@ -1,30 +1,51 @@
-
-// ./app/frames/route.tsx
+/* eslint-disable react/jsx-key */
 import { createFrames, Button } from "frames.js/next";
 
-const frames = createFrames();
+const totalPages = 5;
 
-const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_APP_URL;
+const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_APP_URL || '';
+
+export const frames = createFrames({
+
+    initialState: {
+        pageIndex: 0,
+    },
+});
+
 const handleRequest = frames(async (ctx) => {
+    const pageIndex = Number(ctx.searchParams.pageIndex || 0);
+
+
+    const imageUrl = `https://picsum.photos/seed/frames.js-${pageIndex}/300/200`;
+
     return {
         image: (
-            <div tw="bg-purple-800 text-white w-full h-full justify-center items-center flex">
-                This is rendered as an image
+            <div tw="flex flex-col">
+                <img width={300} height={200} src={imageUrl} alt="Image" />
+                <div tw="flex">
+                    This is slide {pageIndex + 1} / {totalPages}
+                </div>
             </div>
         ),
         buttons: [
-            <Button action="post" target={{ query: { value: "Yes" } }}>
-                {`< Back`}
+            <Button
+                action="post"
+                target={`${NEXT_PUBLIC_URL}/frames/?pageIndex=${(pageIndex - 1) % totalPages}`}
+            >
+                ←
             </Button>,
             <Button action="tx" target={`${NEXT_PUBLIC_URL}/api/txdata`} >
-                Buy a unit
+                Buy it
             </Button >,
-            <Button action="post" target={{ query: { value: "No" } }}>
-                {`Next >`}
-            </Button>,
+            <Button
+                action="post"
+                target={`${NEXT_PUBLIC_URL}/frames/?pageIndex=${(pageIndex + 1) % totalPages}`}
+            >
+                →
+            </Button >,
         ],
+        textInput: "Type something!",
     };
 });
-
 export const GET = handleRequest;
 export const POST = handleRequest;
